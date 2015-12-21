c27c80d9ec7c7d258e8c8e7088c69ae7a0fe46a8
k3nz0 <k3nz09p@gmail.com>
2015-12-21 17:35:53 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+
+=======
+# pixel-ART
+>>>>>>> 189476d76365c2949edd1b97f47e1e531dac22ec
+
+Pixel-ART your friends' faces.
+
+
+# Usage :
+
+Place picture.jpg in the "pics/" folder. 
+
+Run : 
+
+python faces.py
+
+Make sure : 
+	+ picture.jpg exists in your "pics/" folder.
+ 	+ You have reading and writing permissions in "pics/" folder.
+
+You'll find the result in your "pics/tmp_picture.jpg" file.
+
+ 
+If you want to add awesome eyes to your picture just run :
+
+python faces.py picture.jpg eye.png
+
+
+# Quick description of the algorithm:  
+
+The technique used to pixel art faces is simple and yet quite effective.
+
+We colored each grid of N x N pixels with the same color. (N can be changed in faces.py)
+
+
+For face and eyes detection we used the great OpenCV library.
+
+For each face we found on the given picture, we wisely applied eyes detection.
+(Since we usually find eyes on the face :v )  
+
+If this script puts an eye on your mouth don't blame me. Instead, blame OpenCV.
+If this script doesn't put any eye on your face; it doesn't mean you don't have eyes.
+It simply means that OpenCV didn't find them or they were judged to be 
+too small by the script.
+
+Have fun Pixel-Arting your friends' faces.
+
+More stuff can be found on k3nz0.com
---
+++ faces.py
@@ -0,0 +1,167 @@
+import numpy as np
+import cv2
+import Image
+from random import *
+from func import *
+import sys
+
+# ! ADJUST GRID PIXELS
+N = 20
+
+if(len(sys.argv) != 2 and len(sys.argv) != 3):
+	print("\nScript must be run in this way :\n\npython " + sys.argv[0] + " picture.jpg")
+	print("\n\nIf you want to add awesome eyes to your picture just run :")
+	print("\npython faces.py picture.jpg eye.png\n\n")
+	print("Try again !")
+	exit(0)
+
+fName = sys.argv[1]
+dirFile = "pics/"
+ff = dirFile + fName
+eyeFile = "eyes/eye.png"
+
+
+if(len(sys.argv) == 3):
+	eyeFile = "eyes/" + sys.argv[2]
+
+face_cascade = cv2.CascadeClassifier('xml/haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface.xml')
+eye_cascade = cv2.CascadeClassifier('xml/haarcascade_eye.xml')
+
+
+img = cv2.imread(ff)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+print("Faces detected : "  + str(len(faces)))
+#print(faces)
+
+finalEyes = []
+
+for (x, y, w, h) in faces:
+	cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+	roi_gray = gray[y: y + h, x: x + w]
+	roi_color = img[y: y + h, x: x + w]
+	eyes = eye_cascade.detectMultiScale(roi_gray)
+	print(eyes)
+	maxS = twoMaxs(eyes)
+	cnt = 0
+	
+	for (ex, ey, ew, eh) in eyes:
+		if(len(eyes) < 2):
+			continue
+		if( (cnt == maxS[0]) or (cnt == maxS[1]) ):	
+			#if(ew > 21): # magic
+			finalEyes.append((ex + x, ey + y, ew, eh))
+			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		cnt += 1
+
+
+finalEyes.sort(key=lambda tup: tup[0])
+
+print("Eyes detected : "  + str(len(finalEyes)))
+#print(finalEyes)
+
+
+source = Image.open(ff)
+
+width = source.size[0]
+height = source.size[1]
+
+
+visited = [[0 for x in range(width)] for y in range(height)]
+grid = [[ [0, 0, 0] for x in range(width)] for y in range(height)]
+
+data = list(source.getdata())
+
+x = -1
+y = 0
+for k in range(len(data)):
+	if(k % width == 0):
+		x += 1
+		y = 0
+
+	grid[x][y][0] = data[k][0]
+	grid[x][y][1] = data[k][1]
+	grid[x][y][2] = data[k][2]	
+	y += 1
+
+# Generating eyeGrid
+
+
+
+eyeSource = Image.open(eyeFile)
+eyeW = eyeSource.size[0]
+eyeH = eyeSource.size[1]
+
+#print("EYE : ", eyeW, eyeH)
+
+eyeGrid = [[ [0, 0, 0] for x in range(eyeW)] for y in range(eyeH)]
+eyeData = list(eyeSource.getdata())
+x = -1
+y = 0
+
+for k in range(len(eyeData)):
+	if(k % eyeW == 0):
+		x += 1
+		y = 0
+	eyeGrid[x][y][0] = eyeData[k][0]
+	eyeGrid[x][y][1] = eyeData[k][1]
+	eyeGrid[x][y][2] = eyeData[k][2]
+	y += 1
+
+# ---- 
+
+
+
+
+img = Image.new( 'RGB', (width, height), "white") # Because white is good
+pixels = img.load()
+
+currEye = 0
+for i in range(img.size[0]):
+	for j in range(img.size[1]):
+		if(len(sys.argv) == 3):
+			if(currEye < len(finalEyes)):
+				# centering the eyes:
+				startPixW = finalEyes[currEye][0] + abs(eyeW - finalEyes[currEye][2]) / 2
+				startPixH = finalEyes[currEye][1] + abs(eyeH - finalEyes[currEye][3]) / 2
+				if((j == startPixH) and (i == startPixW)):
+					print(finalEyes[currEye][0], finalEyes[currEye][1])
+					print(startPixW, startPixH)
+					currEye += 1
+					for k in range(eyeW):
+						xx = i + k
+						for l in range(eyeH):
+							yy = j + l
+							r = eyeGrid[l][k][0]
+							g = eyeGrid[l][k][1]
+							b = eyeGrid[l][k][2]
+							if((yy < height) and (xx < width)):
+								pixels[xx, yy] = (r, g, b)
+								visited[yy][xx] = 1
+
+		if(visited[j][i]):
+			continue
+		r = grid[j][i][0]
+		g = grid[j][i][1]
+		b = grid[j][i][2]
+
+		for k in range(N):
+
+			xx = i + k
+			for l in range(N):
+				yy = j + l
+				if((yy < height) and (xx < width)):
+					pixels[xx, yy] = (r, g, b)
+					visited[yy][xx] = 1				
+		#pixels[i, j] = (grid[j][i][0], grid[j][i][1], grid[j][i][2]) 
+
+toSave = "tmp_" + fName
+
+img.save(dirFile + toSave)
+print("Picture saved : " + dirFile + toSave)
+img.show()
+#print(finalEyes)
+print("END.")
